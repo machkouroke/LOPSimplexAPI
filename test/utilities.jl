53a57d21776297f222234bb585609bc444e94147
machkouroke <machkour20.mo1@gmail.com>
2022-11-20 11:27:21 +0100

    `🚨 test` Add of test for utilities dunction to build simplex array

--- test/utilities.jl
+++ test/utilities.jl
@@ -76,3 +76,23 @@ end
         @test answer == true_answer
     end
 end
+
+@testset "variable_name_builder" begin
+    A = Float64[-1 1 1
+            1 1 0
+            2 5 0]
+    answer = variable_name_builder(A)
+    true_answer = ["x_1", "x_2", "x_3", "e_1", "e_2", "e_3", "a_1", "a_2", "a_3"]
+    @test answer == true_answer
+end
+
+@testset "in_base_finder" begin
+    @testset "All is less than" begin
+        A = Float64[-1 1 1
+            1 1 0
+            2 5 0]
+        answer = in_base_finder(["<=" for i in 1:size(A)[1]])
+        true_answer = ["e_1", "e_2", "e_3"]
+        @test answer == true_answer
+    end
+end
