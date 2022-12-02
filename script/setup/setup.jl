dbbfd3ce4dc3555b36dadd366aa53e7f99f5ce16
machkouroke <machkour20.mo1@gmail.com>
2022-12-02 01:41:20 +0000

    refactor Deletion of unused file

--- script/setup/setup.jl
+++
@@ -1,6 +0,0 @@
-ENV["PYTHON"] = "/usr/local/bin/python"
-using Pkg
-println("Add of PyCall")
-Pkg.add("PyCall")
-println("Add of LOPSimplex")
-Pkg.add(url="https://github.com/machkouroke/LOPSimplex.jl.git")
