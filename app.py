fa8676f392584c9032de0937f1c0c6253b89302b
OKE   Machkour <40785379+machkouroke@users.noreply.github.com>
2022-12-02 13:36:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ def create_app():
 
     @app.route('/')
     def hello_world():  # put application's code here
-        return 'hello world'
+        return 'Simplex API'
     setup_error_template(app)
 
     CORS(app, resources={r"/*": {"origins": "*"}})
